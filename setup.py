9dee9d6ba135ccba01ed4913b30a51a9ad9b326f
Andrea Grandi <a.grandi@gmail.com>
2014-04-09 12:30:11 +0100

    Fix a typo and project description

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='dict-utils',
     version='0.1',
-    url='https://www.github.com/andreagrandi/dicts-utils/',
+    url='https://www.github.com/andreagrandi/dict-utils/',
     author='Andrea Grandi',
     author_email='a.grandi@gmail.com',
-    description='Simple framework for creating REST APIs',
+    description='Set of utilities and accessory methods to work with Python dicts.',
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
