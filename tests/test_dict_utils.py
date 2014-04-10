7392176a7d1f1f5ea351f3c977a5b4866e40a561
Andrea Grandi <a.grandi@gmail.com>
2014-04-10 14:48:02 +0100

    Add a test case for dictionaries inside a list

--- tests/test_dict_utils.py
+++ tests/test_dict_utils.py
@@ -9,6 +9,11 @@ class DictUtilsTestCase(unittest.TestCase):
         found_value = dict_utils.dict_search_value(dict_1, 'name')
         self.assertEqual(found_value, 'Joe', 'Key not found in the given dict')
 
+    def test_dict_search_found_list(self):
+        dict_1 = {'first_level': [{'second_level': {'name': 'Joe', 'age': 30}}]}
+        found_value = dict_utils.dict_search_value(dict_1, 'name')
+        self.assertEqual(found_value, 'Joe', 'Key not found in the given dict')
+
     def test_dict_search_not_found(self):
         dict_1 = {'first_level': {'second_level': {'name': 'Joe', 'age': 30}}}
         found_value = dict_utils.dict_search_value(dict_1, 'address')
